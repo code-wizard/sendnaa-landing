82b0fff60d8f8b33633f76a812cd75482d7a3415
ebuka <amaefulacj@gmail.com>
2022-05-01 06:44:08 +0100

    add postgres db

--- sendnaa/settings.py
+++ sendnaa/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'sendnaa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'c99a3a52aad5ebee6a657651c2e2378e',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
